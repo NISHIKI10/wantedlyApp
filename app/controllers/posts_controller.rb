9ec4210469f2c2884f49962aa606c13b8e9fa5da
にしき <nishiki@mba.local>
2020-02-01 15:06:15 +0900

    postコントローラー index + postとcompanyのアソシエーション

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,25 @@
 class PostsController < ApplicationController
   before_action :authenticate_company!, except: [:index] 
 
-  # def index
-  # end
+  def index
+    @posts = Post.all
+  end
 
   def new
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
+    @post.company_id = current_company.id
+    @post.save
+    redirect_to posts_path
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:image, :title, :content)
+    end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   }
   resources :companies
   resources :posts
+  root 'posts#index'
 end
